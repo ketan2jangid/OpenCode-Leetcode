49a43514c8bd096a66503b3e04841f94929be7a1
ketanjangid <92528845+ketanjangid@users.noreply.github.com>
2022-08-18 00:41:32 +0530

    Update code.cpp

--- 1679. Max Number of K-Sum Pairs/code.cpp
+++ 1679. Max Number of K-Sum Pairs/code.cpp
@@ -18,3 +18,28 @@ public:
         return count;
     }
 };
+
+//Using two pointers - O(n)
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count= 0;
+        
+        sort(nums.begin(), nums.end());
+        int l = 0;
+        int r = n-1;
+        while(l<r){
+            if(nums[l]+nums[r]==k){
+                count++;
+                l++;
+                r--;
+            }else if(nums[l]+nums[r]<k)
+                l++;
+            else
+                r--;
+        }
+        
+        return count;
+    }
+};
