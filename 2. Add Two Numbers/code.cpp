3246322a36418dd918b7f72a4b4c222c93c5ba86
ketanjangid <92528845+ketanjangid@users.noreply.github.com>
2022-08-20 14:27:44 +0530

    Update code.cpp

--- 2. Add Two Numbers/code.cpp
+++ 2. Add Two Numbers/code.cpp
@@ -52,3 +52,45 @@ public:
     }
 };
 
+
+//Using Recursion
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+        return add(l1,l2,carry);
+    }
+    
+    ListNode* add(ListNode* l1, ListNode* l2, int carry){
+        ListNode* ptr;
+        if(!l1 && !l2){
+            if(carry){
+                ptr = new ListNode(carry);
+                return ptr;
+            }else
+                return NULL;
+        }
+        
+        if(!l1){
+            ptr = new ListNode;
+            ptr->val = (l2->val+carry)%10;
+            carry = (l2->val+carry)/10;
+            
+            ptr->next = add(l1,l2->next,carry);
+        }else if(!l2){
+            ptr = new ListNode;
+            ptr->val = (l1->val+carry)%10;
+            carry = (l1->val+carry)/10;
+            
+            ptr->next = add(l1->next,l2,carry);
+        }else{
+            ptr = new ListNode;
+            ptr->val = (l1->val+l2->val+carry)%10;
+            carry = (l1->val+l2->val+carry)/10;
+            
+            ptr->next = add(l1->next,l2->next,carry);
+        }
+            
+        return ptr;
+    }
+};
