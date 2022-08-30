cb3e451b6ecd829ed3a7738c8c616f618d3afa6e
ketanjangid <92528845+ketanjangid@users.noreply.github.com>
2022-08-30 18:02:29 +0530

    Update code.cpp

--- 200. Number of Islands/code.cpp
+++ 200. Number of Islands/code.cpp
@@ -50,3 +50,44 @@ public:
     }
 };
 
+//Using Depth First Search
+class Solution {
+public:
+    int m,n;
+    int numIslands(vector<vector<char>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        
+        int islands = 0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]=='1'){
+                    grid[i][j] = '0';
+                    islanddfs(grid,i,j);
+                    islands++;
+                }
+            }
+        }
+        
+        return islands;
+    }
+    
+    void islanddfs(vector<vector<char>>& grid, int i, int j){
+        if(i-1>=0 && grid[i-1][j]=='1'){
+            grid[i-1][j] = '0';
+            islanddfs(grid,i-1,j);
+        }
+        if(i+1<m && grid[i+1][j]=='1'){
+            grid[i+1][j] = '0';
+            islanddfs(grid,i+1,j);
+        }
+        if(j-1>=0 && grid[i][j-1]=='1'){
+            grid[i][j-1] = '0';
+            islanddfs(grid,i,j-1);
+        }
+        if(j+1<n && grid[i][j+1]=='1'){
+            grid[i][j+1] = '0';
+            islanddfs(grid,i,j+1);
+        }
+    }
+};
