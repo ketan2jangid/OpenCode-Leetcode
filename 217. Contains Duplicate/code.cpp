55a08c6de99e4d087506185cfaac35ad4f45fc5c
ketanjangid <92528845+ketanjangid@users.noreply.github.com>
2022-09-01 19:50:33 +0530

    Create code.cpp

---
+++ 217. Contains Duplicate/code.cpp
@@ -0,0 +1,33 @@
+//using hashmap
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        
+        unordered_map<int,int> m;
+        for(auto x: nums)
+            m[x]++;
+        
+        for(auto p: m)
+            if(p.second>1)
+                return true;
+        
+        return false;
+    }
+};
+
+//using sorting
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1)    return false;
+        
+        sort(nums.begin(), nums.end());
+        for(int i=1; i<n; i++)
+            if(nums[i]==nums[i-1])
+                return true;
+        
+        return false;
+    }
+};
