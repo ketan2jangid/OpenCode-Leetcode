83d47e588af5959ab26f1503430e7e427aa13e45
ketanjangid <92528845+ketanjangid@users.noreply.github.com>
2022-09-04 14:17:10 +0530

    Update code.cpp

--- 88. Merge Sorted Array/code.cpp
+++ 88. Merge Sorted Array/code.cpp
@@ -9,3 +9,24 @@ public:
     }
 };
 
+//in O(m+n)
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i=m-1, k=m+n-1; i>=0; i--, k--)
+            nums1[k] = nums1[i];
+
+        int i,j,k;
+        i = n;
+        j = k = 0;
+        while(i<m+n && j<n){
+            if(m && nums1[i]<nums2[j])
+                nums1[k++] = nums1[i++];
+            else
+                nums1[k++] = nums2[j++];
+        }
+
+        while(j<n)
+            nums1[k++] = nums2[j++];
+    }
+};
