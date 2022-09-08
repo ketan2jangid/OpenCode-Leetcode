2bfcf85d0816d9e222a0adf8afa7da19c4ec9ca4
ketanjangid <92528845+ketanjangid@users.noreply.github.com>
2022-09-08 09:41:34 +0530

    Update code.cpp

--- 606. Construct String from Binary Tree/code.cpp
+++ 606. Construct String from Binary Tree/code.cpp
@@ -1,14 +1,17 @@
-//using recursion
 class Solution {
 public:
     string tree2str(TreeNode* root) {
         string toadd = to_string(root->val);
         
-        if(root->left){
-            toadd = toadd + "(" + tree2str(root->left) + ")";
-        }
-        if(root->right){
+        if(root->right){            
+            if(root->left)
+                toadd = toadd + "(" + tree2str(root->left) + ")";
+            else
+                toadd += "()";
             toadd = toadd + "(" + tree2str(root->right) + ")";
+        }else{
+            if(root->left)
+                toadd = toadd + "(" + tree2str(root->left) + ")";
         }
         
         return toadd;
