2e4e66f058e6aa21fb397bf6c0a7e239c3fb0c3c
ketanjangid <92528845+ketanjangid@users.noreply.github.com>
2022-09-12 19:03:03 +0530

    Update code.cpp

--- 350. Intersection of Two Arrays II/code.cpp
+++ 350. Intersection of Two Arrays II/code.cpp
@@ -20,3 +20,28 @@ public:
     }
 };
 
+//using sort - O(n*logn)
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        
+        int l = 0;
+        int r = 0;
+        
+        vector<int> intersection;
+        while(l<nums1.size() && r<nums2.size()){
+            if(nums1[l]==nums2[r]){
+                intersection.push_back(nums1[l]);
+                l++;
+                r++;
+            }else if(nums1[l]<nums2[r])
+                l++;
+            else
+                r++;
+        }
+        
+        return intersection;
+    }
+};
