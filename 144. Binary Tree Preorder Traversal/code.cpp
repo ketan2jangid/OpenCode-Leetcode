936eb39285f55210aab73d00f99712a1dfba5aa9
ketanjangid <92528845+ketanjangid@users.noreply.github.com>
2022-09-12 20:32:12 +0530

    Update code.cpp

--- 144. Binary Tree Preorder Traversal/code.cpp
+++ 144. Binary Tree Preorder Traversal/code.cpp
@@ -1,4 +1,5 @@
-//TC - O(n)
+//recursive
+//TC - O(n), SC - O(1)
 class Solution {
 public:
     vector<int> ans;
@@ -17,5 +18,30 @@ public:
         
         preorder(root->left);
         preorder(root->right);
+    }
+};
+
+//iterative
+//TC - O(n), SC - O(n)
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(!root)    return ans;
+        stack<TreeNode*> stk;
+        
+        stk.push(root);
+        TreeNode* temp;
+        while(!stk.empty()){
+            temp = stk.top();
+            stk.pop();
+            
+            ans.push_back(temp->val);
+            
+            if(temp->right) stk.push(temp->right);
+            if(temp->left)  stk.push(temp->left);
+        }
+        
+        return ans;
     }
 };
