3880511cd7a29482bb47630b763094b3d94fec0e
ketanjangid <92528845+ketanjangid@users.noreply.github.com>
2022-09-14 00:05:31 +0530

    Create code.cpp

---
+++ 226. Invert Binary Tree/code.cpp
@@ -0,0 +1,16 @@
+//recursive - O(n)
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root)   return root;
+        
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        
+        invertTree(root->left);
+        invertTree(root->right);
+        
+        return root;
+    }
+};
