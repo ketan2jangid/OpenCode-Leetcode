cbaf41da9d9f863c0811463c0439295dfbce54f3
ketanjangid <92528845+ketanjangid@users.noreply.github.com>
2022-09-14 10:52:15 +0530

    Update code.cpp

--- 83. Remove Duplicates from Sorted List/code.cpp
+++ 83. Remove Duplicates from Sorted List/code.cpp
@@ -18,3 +18,29 @@ public:
         return head;
     }
 };
+
+//recursive
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head)   return head;
+        
+        remove(head, head->next);
+        
+        return head;
+    }
+    
+    void remove(ListNode* ptr, ListNode* ahead){
+        if(!ahead){
+            ptr->next = ahead;
+            return;
+        }
+        
+        if(ptr->val!=ahead->val){
+            ptr->next = ahead;
+            ptr = ptr->next;
+        }
+        
+        return remove(ptr, ahead->next);
+    }
+};
