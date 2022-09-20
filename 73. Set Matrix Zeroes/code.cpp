a81a15defb1f829da422e5d6c6e565dc15c2e2ab
ketanjangid <92528845+ketanjangid@users.noreply.github.com>
2022-09-20 17:35:02 +0530

    Update code.cpp

--- 73. Set Matrix Zeroes/code.cpp
+++ 73. Set Matrix Zeroes/code.cpp
@@ -5,6 +5,48 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         
+        bool toprow = false;
+        bool topcol = false;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 || j==0){
+                    if(i==0 && matrix[i][j]==0){
+                        toprow = true;
+                    }
+                    if(j==0 && matrix[i][j]==0){
+                        topcol = true;
+                    }
+                }else if(matrix[i][j]==0 && i!=0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for(int i=m-1; i>0; i--)
+            for(int j=n-1; j>0; j--)
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                    matrix[i][j] = 0;
+            
+        
+        if(toprow)
+            for(int j=0; j<n; j++)
+                matrix[0][j] = 0;
+    
+        if(topcol)
+            for(int i=0; i<m; i++)
+                matrix[i][0] = 0;
+        
+    }
+};
+
+//using string
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
         string row = "";
         string col = "";
         
