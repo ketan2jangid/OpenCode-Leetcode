83b902f05a65fb960e18dc2b0d50a9c1b800e79c
ketanjangid <92528845+ketanjangid@users.noreply.github.com>
2022-09-20 15:59:46 +0530

    Create code.cpp

---
+++ 73. Set Matrix Zeroes/code.cpp
@@ -0,0 +1,36 @@
+//SC - O(1), TC - O(m*n)
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        string row = "";
+        string col = "";
+        
+        for(int i=0; i<m; i++)
+            row += '1';
+        for(int i=0; i<n; i++)
+            col += '1';
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j]==0){
+                    row[i] = '0';
+                    col[j] = '0';
+                }
+            }
+        }
+        
+        for(int i=0; i<m; i++)
+            if(row[i]=='0')
+                for(int j=0; j<n; j++)
+                    matrix[i][j] = 0;
+        
+        
+        for(int j=0; j<n; j++)
+            if(col[j]=='0')
+                for(int i=0; i<m; i++)
+                    matrix[i][j] = 0;
+    }
+};
