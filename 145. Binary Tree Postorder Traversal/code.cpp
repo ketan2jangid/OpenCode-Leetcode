54fd41a761bc5e99395fd83675d89266c2bd4b2b
ketanjangid <92528845+ketanjangid@users.noreply.github.com>
2022-10-19 12:31:08 +0530

    Update code.cpp

--- 145. Binary Tree Postorder Traversal/code.cpp
+++ 145. Binary Tree Postorder Traversal/code.cpp
@@ -18,3 +18,32 @@ public:
         ans.push_back(root->val);
     }
 };
+
+//iterative
+//TC - O(n), SC - O(n)
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        if(!root)   return {};
+        
+        vector<int> postorder;
+        stack<TreeNode*> s;
+        
+        s.push(root);
+        TreeNode* temp;
+        
+        while(!s.empty()){
+            temp = s.top();
+            s.pop();
+            postorder.push_back(temp->val);
+            
+            if(temp->left)
+                s.push(temp->left);
+            if(temp->right)
+                s.push(temp->right);
+        }
+        reverse(postorder.begin(), postorder.end());
+        
+        return postorder;
+    }
+};
