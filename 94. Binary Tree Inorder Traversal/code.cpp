56a9783e010a6561cbc473e5b6d0dcffd02b5855
ketanjangid <92528845+ketanjangid@users.noreply.github.com>
2022-10-19 18:03:34 +0530

    Update code.cpp

--- 94. Binary Tree Inorder Traversal/code.cpp
+++ 94. Binary Tree Inorder Traversal/code.cpp
@@ -1,4 +1,5 @@
 //recursive
+//TC - O(n), SC - O(1)
 class Solution {
 public:
     vector<int> ans;
@@ -13,5 +14,33 @@ public:
         inorder(root->left);
         ans.push_back(root->val);
         inorder(root->right);
+    }
+};
+
+//iterative 
+//TC - O(n), SC - O(n)
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorder;
+        stack<TreeNode*> s;
+        
+        if(!root)   return inorder;
+        s.push(root);
+        TreeNode* ptr = root->left;
+        
+        while(!s.empty() || ptr){
+            if(!ptr){
+                ptr = s.top();
+                s.pop();
+                inorder.push_back(ptr->val);
+                ptr = ptr->right;
+            }else{
+                s.push(ptr);
+                ptr = ptr->left;
+            }
+        }
+        
+        return inorder;
     }
 };
